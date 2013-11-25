0d81b5b444ff45d4e4d04b529a50dcab68710cf5
bleen <git.119x@bleen.net>
2013-11-24 21:26:33 -0500

    Adding alter hooks in case a third-perty module wants to attach focal point data to another field or file

---
+++ focal_point.api.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Documentation of Feeds hooks.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter an array of supported field types.
+ *
+ * @param array $supported
+ */
+function hook_focal_point_supported_field_types_alter(&$supported) {
+  $supported[] = 'my_custom_image_type';
+}
+
+/**
+ * Alter an array of supported widget types.
+ *
+ * @param array $supported
+ */
+function hook_focal_point_supported_widget_types_alter(&$supported) {
+  $supported[] = 'mymodule_my_custom_widget_type';
+}
+
+/**
+ * Alter an array of supported file entity types.
+ *
+ * @param array $supported
+ */
+function hook_focal_point_supported_file_types_alter(&$supported) {
+  $supported[] = 'custom_file_entity_type';
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
