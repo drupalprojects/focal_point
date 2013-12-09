842e223483a5bf953b33bd8b01260744b21f54cc
bleen <git.119x@bleen.net>
2013-12-09 13:42:12 -0500

    Removing test for function that no longer exists

--- test/FocalPointTest.php
+++ test/FocalPointTest.php
@@ -6,30 +6,6 @@ include_once __DIR__ . './../focal_point.effects.inc';
 class focalPointTest extends PHPUnit_Framework_TestCase {
 
   /**
-   * @dataProvider fieldSupportedProvider
-   */
-  public function testFieldSupported($field_type, $instance_type, $is_enabled, $require_enabled, $expected) {
-    $field = array(
-      'type' => $field_type,
-    );
-    $instance = array(
-      'settings' => array('focal_point_enabled' => $is_enabled),
-      'widget' => array('type' => $instance_type),
-    );
-
-    $this->assertSame($expected, _focal_point_field_supported($field, $instance, $require_enabled));
-  }
-
-  public function fieldSupportedProvider() {
-    return array(
-      array('image', 'image_image', TRUE, TRUE, TRUE),
-      array('image', 'image_image', FALSE, TRUE, FALSE),
-      array('image', 'not_image', TRUE, TRUE, FALSE),
-      array('not_image', 'image_image', TRUE, TRUE, FALSE),
-    );
-  }
-
-  /**
    * @dataProvider parseFocalPointProvider
    */
   public function testFocalPoint($focal_point, $expected) {
