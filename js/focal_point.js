bf1a28931600af2bd94aef685224e7cb26bb8faf
bleen <git.119x@bleen.net>
2014-06-09 13:11:29 -0400

    Creating a js FocalPoint object

--- js/focal_point.js
+++ js/focal_point.js
@@ -5,7 +5,7 @@
  * Javascript functionality for the focal point widget.
  */
 
-(function($) {
+(function($, Drupal) {
   'use strict';
 
   /**
@@ -26,16 +26,8 @@
         // Set some variables for the different pieces at play.
         var $indicator = $(this);
         var $img = $(this).siblings('img');
-        var $fieldDelta = $(this).attr('data-delta');
-        var $fieldName = $(context).find('#file-entity-add-upload').length > 0 ? 'media' : $(this).attr('data-field-name');
-        var $field = $(".focal-point-" + $fieldName + '-' + $fieldDelta);
-        var $previewLink = $(".focal-point-preview-link-" + $fieldName + '-' + $fieldDelta);
-
-        $indicator.dblclick(function() {
-          $field.closest('.form-item').toggle();
-        });
-
-        $img.css('cursor', 'crosshair');
+        var $field = $(".focal-point-" + $(this).attr('data-field-name') + '-' + $(this).attr('data-delta'));
+        var fp = new Drupal.FocalPoint($indicator, $img, $field);
 
         // Set the position of the indicator on image load and any time the
         // field value changes. We use a bit of hackery to make certain that the
@@ -44,46 +36,11 @@
         // modal windows. See http://goo.gl/s73ge.
         setTimeout(function() {
           $img.one('load', function(){
-            focalPointSetIndicator($indicator, $(this), $field);
+            fp.setIndicator();
           }).each(function() {
             if (this.complete) $(this).load();
           });
         }, 0);
-
-        // Make the focal point indicator draggable and tell it to update the
-        // appropriate field when it is moved by the user.
-        $(this).draggable({
-          containment: $img,
-          stop: function() {
-            var imgOffset = $img.offset();
-            var focalPointOffset = $(this).offset();
-
-            var leftDelta = focalPointOffset.left - imgOffset.left;
-            var topDelta = focalPointOffset.top - imgOffset.top;
-
-            focalPointSet(leftDelta, topDelta, $img, $indicator, $img);
-          }
-        });
-
-        // Allow users to click on the image preview in order to set the focal_point.
-        $img.click(event, function() {
-          focalPointSet(event.offsetX, event.offsetY, $img, $indicator, $img);
-        });
-
-        // Add a change event to the focal point field so it will properly
-        // update the indicator position and the preview link.
-        $field.change(function() {
-          // Update the indicator position in case someone has typed in a value.
-          focalPointSetIndicator($indicator, $img, $(this));
-
-          // Re-jigger the href of the preview link.
-          if ($previewLink.length > 0) {
-            var href = $previewLink.attr('href').split('/');
-            href.pop();
-            href.push(encodeURIComponent($(this).val()));
-            $previewLink.attr('href', href.join('/'));
-          }
-        });
 
       });
     }
@@ -91,27 +48,7 @@
   };
 
   /**
-   * Set the focal point.
-   *
-   * @param int offsetX
-   *   Left offset in pixels.
-   * @param int offsetY
-   *   Top offset in pixels.
-   * @param object $img
-   *   The image jQuery object to which the indicator is attached.
-   * @param object $indicator
-   *   The indicator jQuery object whose position should be set.
-   * @param object $field
-   *   The field jQuery object where the position can be found.
-   */
-  function focalPointSet(offsetX, offsetY, $img, $indicator, $field) {
-    var focalPoint = focalPointCalculate(offsetX, offsetY, $img);
-    $field.val(focalPoint.x + ',' + focalPoint.y).trigger('change');
-    focalPointSetIndicator($indicator, $img, $field);
-  }
-
-  /**
-   * Change the position of the focal point indicator. This may not work in IE7.
+   * Object representing the focal point for a given image.
    *
    * @param object $indicator
    *   The indicator jQuery object whose position should be set.
@@ -120,11 +57,71 @@
    * @param array $field
    *   The field jQuery object where the position can be found.
    */
-  function focalPointSetIndicator($indicator, $img, $field) {
-    var coordinates = $field.val() !== '' && $field.val() !== undefined ? $field.val().split(',') : [50,50];
-    $indicator.css('left', (parseInt(coordinates[0], 10) / 100) * $img.width());
-    $indicator.css('top', (parseInt(coordinates[1], 10) / 100) * $img.height());
-    $field.val(coordinates[0] + ',' + coordinates[1]);
+  Drupal.FocalPoint = function($indicator, $img, $field) {
+    this.$indicator = $indicator;
+    this.$img = $img;
+    this.$field = $field;
+
+    // Make the focal point indicator draggable and tell it to update the
+    // appropriate field when it is moved by the user.
+    this.$indicator.draggable({
+      containment: this.$img,
+      stop: function() {
+        var imgOffset = this.$img.offset();
+        var focalPointOffset = this.$indicator.offset();
+
+        var leftDelta = focalPointOffset.left - imgOffset.left;
+        var topDelta = focalPointOffset.top - imgOffset.top;
+
+        this.set(leftDelta, topDelta);
+      }
+    });
+
+    // Allow users to double-click the indicator to reveal the focal point form
+    // element.
+    this.$indicator.dblclick(function() {
+      this.$field.closest('.form-item').toggle();
+    });
+
+    // Allow users to click on the image preview in order to set the focal_point
+    // and set a cursor.
+    this.$img.click(event, function() {
+      this.set(event.offsetX, event.offsetY);
+    });
+    this.$img.css('cursor', 'crosshair');
+
+    // Add a change event to the focal point field so it will properly update
+    // the indicator position.
+    this.$field.change(function() {
+      // Update the indicator position in case someone has typed in a value.
+      this.setIndicator();
+    });
+
+
+  }
+
+  /**
+   * Set the focal point.
+   *
+   * @param int offsetX
+   *   Left offset in pixels.
+   * @param int offsetY
+   *   Top offset in pixels.
+   */
+  Drupal.FocalPoint.prototype.set = function(offsetX, offsetY) {
+    var focalPoint = this.calculate(offsetX, offsetY);
+    this.$field.val(focalPoint.x + ',' + focalPoint.y).trigger('change');
+    this.setIndicator();
+  }
+
+  /**
+   * Change the position of the focal point indicator. This may not work in IE7.
+   */
+  Drupal.FocalPoint.prototype.setIndicator = function() {
+    var coordinates = this.$field.val() !== '' && this.$field.val() !== undefined ? this.$field.val().split(',') : [50,50];
+    this.$indicator.css('left', (parseInt(coordinates[0], 10) / 100) * this.$img.width());
+    this.$indicator.css('top', (parseInt(coordinates[1], 10) / 100) * this.$img.height());
+    this.$field.val(coordinates[0] + ',' + coordinates[1]);
   }
 
   /**
@@ -134,15 +131,13 @@
    *   Left offset in pixels.
    * @param int offsetY
    *   Top offset in pixels.
-   * @param object $img
-   *   The image jQuery object to which the indicator is attached.
    *
    * @returns object
    */
-  function focalPointCalculate(offsetX, offsetY, $img) {
+  Drupal.FocalPoint.prototype.calculate = function(offsetX, offsetY) {
     var focalPoint = {};
-    focalPoint.x = focalPointRound(100 * offsetX / $img.width(), 0, 100);
-    focalPoint.y = focalPointRound(100 * offsetY / $img.height(), 0, 100);
+    focalPoint.x = this.round(100 * offsetX / this.$img.width(), 0, 100);
+    focalPoint.y = this.round(100 * offsetY / this.$img.height(), 0, 100);
 
     return focalPoint;
   }
@@ -159,11 +154,11 @@
    *
    * @returns int
    */
-  function focalPointRound(value, min, max){
+  Drupal.FocalPoint.prototype.round = function(value, min, max){
     var roundedVal = Math.max(Math.round(value), min);
     roundedVal = Math.min(roundedVal, max);
 
     return roundedVal;
   }
 
-})(jQuery);
+})(jQuery, Drupal);
