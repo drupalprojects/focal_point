aec9e8c817affd6e2b5217d8b577fa9d7ab6bff4
slashrsm <slashrsm@744628.no-reply.drupal.org>
2016-05-19 14:54:02 -0400

    Issue #2626950 by bleen, slashrsm, slasher13: Depend on Crop API for storage

--- tests/src/Unit/Effects/FocalPointEffectsTest.php
+++ tests/src/Unit/Effects/FocalPointEffectsTest.php
@@ -7,20 +7,34 @@
 
 namespace Drupal\Tests\focal_point\Unit\Effects;
 
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Image\ImageInterface;
+use Drupal\crop\CropInterface;
+use Drupal\crop\CropStorageInterface;
+use Drupal\focal_point\Plugin\ImageEffect\FocalPointCropImageEffect;
 use Drupal\Tests\UnitTestCase;
 use Drupal\focal_point\FocalPointEffectBase;
+use Psr\Log\LoggerInterface;
 
 /**
  * Tests the Focal Point image effects.
  *
  * @group Focal Point
- * @group Drupal
  *
- * @see \Drupal\focal_point\FocalPointEffectBase
+ * @coversDefaultClass \Drupal\focal_point\FocalPointEffectBase
  */
 class FocalPointEffectsTest extends UnitTestCase {
 
   /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * @covers ::calculateResizeData
+   *
    * @dataProvider calculateResizeDataProvider
    */
   public function testCalculateResizeData($image_width, $image_height, $crop_width, $crop_height, $expected) {
@@ -33,56 +47,47 @@ class FocalPointEffectsTest extends UnitTestCase {
    * @see FocalPointEffectsTest::testCalculateResizeData()
    */
   public function calculateResizeDataProvider() {
-    return array(
-      array(640, 480, 300, 100, array('width' => 300, 'height' => 225)), // Horizontal image with horizontal crop.
-      array(640, 480, 100, 300, array('width' => 400, 'height' => 300)), // Horizontal image with vertical crop.
-      array(480, 640, 300, 100, array('width' => 300, 'height' => 400)), // Vertical image with horizontal crop.
-      array(480, 640, 100, 300, array('width' => 225, 'height' => 300)), // Vertical image with vertical crop.
-      array(640, 480, 3000, 1000, array('width' => 3000, 'height' => 2250)), // Horizontal image with too large crop.
-      array(1920, 1080, 400, 300, array('width' => 533, 'height' => 300)), // Image would be too narrow to crop after resize.
-      array(200, 400, 1000, 1000, array('width' => 1000, 'height' => 2000)), // Image would be too short to crop after resize.
-    );
+    $data = [];
+    $data['horizontal_image_horizontal_crop'] = [640, 480, 300, 100, ['width' => 300, 'height' => 225]];
+    $data['horizontal_image_vertical_crop'] = [640, 480, 100, 300, ['width' => 400, 'height' => 300]];
+    $data['vertical_image_horizontal_crop'] = [480, 640, 300, 100, ['width' => 300, 'height' => 400]];
+    $data['vertical_image_vertical_crop'] = [480, 640, 100, 300, ['width' => 225, 'height' => 300]];
+    $data['horizontal_image_too_large_crop'] = [640, 480, 3000, 1000, ['width' => 3000, 'height' => 2250]];
+    $data['image_too_narrow_to_crop_after_resize'] = [1920, 1080, 400, 300, ['width' => 533, 'height' => 300]];
+    $data['image_too_short_to_crop_after_resize'] = [200, 400, 1000, 1000, ['width' => 1000, 'height' => 2000]];
+    return $data;
   }
 
   /**
-   * @dataProvider calculateCropDataProvider
-   */
-  public function testCalculateCropData($focal_point, $image_width, $image_height, $crop_width, $crop_height, $expected) {
-    $this->assertSame($expected, FocalPointEffectBase::calculateCropData($focal_point, $image_width, $image_height, $crop_width, $crop_height));
-  }
-
-  /**
-   * Data provider for testCalculateCropData().
+   * @covers ::calculateAnchor
    *
-   * @see FocalPointEffectsTest::testCalculateCropData()
-   */
-  public function calculateCropDataProvider() {
-    return array(
-      array('50,50', 640, 480, 300, 100, array('width' => 300, 'height' => 100, 'x' => 170, 'y' => 190)),
-      array('50,50', 640, 480, 100, 300, array('width' => 100, 'height' => 300, 'x' => 270, 'y' => 90)),
-      array('50,50', 480, 640, 300, 100, array('width' => 300, 'height' => 100, 'x' => 90, 'y' => 270)),
-      array('50,50', 480, 640, 100, 300, array('width' => 100, 'height' => 300, 'x' => 190, 'y' => 170)),
-      array('50,50', 1920, 1080, 400, 300, array('width' => 400, 'height' => 300, 'x' => 760, 'y' => 390)),
-
-      array('invalid', 640, 480, 300, 100, array('width' => 300, 'height' => 100, 'x' => 170, 'y' => 190)),
-      array('invalid', 640, 480, 100, 300, array('width' => 100, 'height' => 300, 'x' => 270, 'y' => 90)),
-      array('invalid', 480, 640, 300, 100, array('width' => 300, 'height' => 100, 'x' => 90, 'y' => 270)),
-      array('invalid', 480, 640, 100, 300, array('width' => 100, 'height' => 300, 'x' => 190, 'y' => 170)),
-      array('invalid', 1920, 1080, 400, 300, array('width' => 400, 'height' => 300, 'x' => 760, 'y' => 390)),
-
-      array('75,25', 640, 480, 300, 100, array('width' => 300, 'height' => 100, 'x' => 330, 'y' => 70)),
-      array('75,25', 640, 480, 100, 300, array('width' => 100, 'height' => 300, 'x' => 430, 'y' => 0)),
-      array('75,25', 480, 640, 300, 100, array('width' => 300, 'height' => 100, 'x' => 180, 'y' => 110)),
-      array('75,25', 480, 640, 100, 300, array('width' => 100, 'height' => 300, 'x' => 310, 'y' => 10)),
-      array('75,25', 1920, 1080, 400, 300, array('width' => 400, 'height' => 300, 'x' => 1240, 'y' => 120)),
-    );
-  }
-
-  /**
    * @dataProvider calculateAnchorProvider
    */
-  public function testCalculateAnchor($image_size, $crop_size, $focal_point_offset, $expected) {
-    $this->assertSame($expected, FocalPointEffectBase::calculateAnchor($image_size, $crop_size, $focal_point_offset));
+  public function testCalculateAnchor($image_size, $crop_size, $focal_point_anchor, $expected) {
+    $logger = $this->prophesize(LoggerInterface::class);
+    $crop_storage = $this->prophesize(CropStorageInterface::class);
+    $immutable_config = $this->prophesize(ImmutableConfig::class);
+
+    $effect = new FocalPointCropImageEffect([], 'plugin_id', [], $logger->reveal(), $crop_storage->reveal(), $immutable_config->reveal());
+
+    $image = $this->prophesize(ImageInterface::class);
+    $image->getWidth()->willReturn($image_size[0]);
+    $image->getHeight()->willReturn($image_size[1]);
+
+    $crop = $this->prophesize(CropInterface::class);
+    $crop->anchor()->willReturn([
+      'x' => $focal_point_anchor[0],
+      'y' => $focal_point_anchor[1],
+    ]);
+    $crop->size()->willReturn([
+      'width' => $crop_size[0],
+      'height' => $crop_size[1],
+    ]);
+
+    $effect_reflection = new \ReflectionClass(FocalPointCropImageEffect::class);
+    $method = $effect_reflection->getMethod('calculateAnchor');
+    $method->setAccessible(TRUE);
+    $this->assertSame($expected, $method->invokeArgs($effect, [$image->reveal(), $crop->reveal()]));
   }
 
   /**
@@ -91,13 +96,18 @@ class FocalPointEffectsTest extends UnitTestCase {
    * @see FocalPointEffectsTest::testCalculateAnchor()
    */
   public function calculateAnchorProvider() {
-    return array(
-      array(640, 300, 50, 170),
-      array(640, 300, 80, 340),
-      array(640, 300, 10, 0),
-      array(640, 640, 640, 0),
-      array(640, 800, 50, 0),
-    );
+    $data = [];
+    $data['crop_fits_within_image_even'] = [[1000, 100], [30, 20], [250, 50], ['x' => 235, 'y' => 40]];
+    $data['crop_fits_within_image_odd'] = [[1000, 100], [35, 25], [313, 49], ['x' => 295, 'y' => 36]];
+    $data['crop_does_not_fall_off_image_top'] = [[1000, 100], [30, 40], [313, 10], ['x' => 298, 'y' => 0]];
+    $data['crop_does_not_fall_off_image_bottom'] = [[1000, 100], [50, 50], [900, 90], ['x' => 875, 'y' => 50]];
+    $data['crop_does_not_fall_off_image_left'] = [[1000, 100], [50, 50], [10, 50], ['x' => 0, 'y' => 25]];
+    $data['crop_does_not_fall_off_image_right'] = [[1000, 100], [50, 50], [975, 60], ['x' => 950, 'y' => 35]];
+    $data['crop_does_not_fall_off_image_top_left'] = [[1000, 100], [50, 50], [0, 0], ['x' => 0, 'y' => 0]];
+    $data['crop_does_not_fall_off_image_top_right'] = [[1000, 100], [50, 50], [1000, 5], ['x' => 950, 'y' => 0]];
+    $data['crop_does_not_fall_off_image_bottom_left'] = [[1000, 100], [75, 75], [0, 100], ['x' => 0, 'y' => 25]];
+    $data['crop_does_not_fall_off_image_bottom_right'] = [[1000, 100], [50, 50], [1000, 100], ['x' => 950, 'y' => 50]];
+    return $data;
   }
 
 }
