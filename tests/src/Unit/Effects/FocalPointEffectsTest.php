0c6078672a86ea5b009932b1fcee505131cfe84b
bleen18 <bleen18@77375.no-reply.drupal.org>
2016-06-16 22:06:39 -0400

    Issue #2742385 by bleen: Improve test coverage

--- tests/src/Unit/Effects/FocalPointEffectsTest.php
+++ tests/src/Unit/Effects/FocalPointEffectsTest.php
@@ -11,13 +11,10 @@ use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Image\ImageInterface;
 use Drupal\crop\CropInterface;
 use Drupal\crop\CropStorageInterface;
-use Drupal\Core\Entity\EntityTypeManager;
-use Drupal\focal_point\FocalPointManager;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\focal_point\Plugin\ImageEffect\FocalPointCropImageEffect;
-use Drupal\Tests\UnitTestCase;
 use Drupal\focal_point\FocalPointEffectBase;
 use Psr\Log\LoggerInterface;
+use Drupal\Tests\focal_point\Unit\FocalPointUnitTestCase;
 
 /**
  * Tests the Focal Point image effects.
@@ -26,13 +23,26 @@ use Psr\Log\LoggerInterface;
  *
  * @coversDefaultClass \Drupal\focal_point\FocalPointEffectBase
  */
-class FocalPointEffectsTest extends UnitTestCase {
+class FocalPointEffectsTest extends FocalPointUnitTestCase {
 
   /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
+  }
+
+  /**
+   * @covers ::__construct
+   */
+  public function testEffectConstructor() {
+    $logger = $this->prophesize(LoggerInterface::class);
+    $crop_storage = $this->prophesize(CropStorageInterface::class);
+    $focal_point_config = $this->prophesize(ImmutableConfig::class);
+
+    $effect = new FocalPointCropImageEffect([], 'plugin_id', [], $logger->reveal(), $crop_storage->reveal(), $focal_point_config->reveal());
+    $this->assertAttributeEquals($crop_storage->reveal(), 'cropStorage', $effect);
+    $this->assertAttributeEquals($focal_point_config->reveal(), 'focalPointConfig', $effect);
   }
 
   /**
@@ -70,17 +80,6 @@ class FocalPointEffectsTest extends UnitTestCase {
     $logger = $this->prophesize(LoggerInterface::class);
     $crop_storage = $this->prophesize(CropStorageInterface::class);
     $immutable_config = $this->prophesize(ImmutableConfig::class);
-
-    $entity_type_manager = $this->prophesize(EntityTypeManager::class);
-    $entity_type_manager->getStorage('crop')->willReturn($crop_storage);
-
-    $container = $this->prophesize(ContainerInterface::class);
-    $container->get('entity_type.manager')->willReturn($entity_type_manager);
-
-    $focal_point_manager = new FocalPointManager($entity_type_manager->reveal());
-    $container->get('focal_point.manager')->willReturn($focal_point_manager);
-
-    \Drupal::setContainer($container->reveal());
 
     $image = $this->prophesize(ImageInterface::class);
     $image->getWidth()->willReturn($resized_image_size['width']);
